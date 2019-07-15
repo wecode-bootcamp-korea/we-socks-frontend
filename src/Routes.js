d2bcdb7a9bb023ad90f70df1f6d7b685856e38d5
Byeongjin Kang <byeongjinkang@Byeongjinui-MacBookPro.local>
2019-07-15 14:46:01 +0900

    setup complete

---
+++ src/Routes.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Main from ".Main";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+class Routes extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/login" component={Main} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default Routes;

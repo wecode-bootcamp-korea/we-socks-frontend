548d86c37855f46a7d9c939b79564c5b0c63ddba
Georgee-lee <rjsahdia3@gmail.com>
2019-07-15 23:29:45 +0900

    ready to make list page

---
+++ src/Pages/Itemlist/Itemlist.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "Components/Header";
+import "./itemlist.scss";
+
+const itemlist = props => {
+  console.log(props.location.query);
+
+  return (
+    <div>
+      <p>123</p>
+    </div>
+  );
+};
+
+export default itemlist;

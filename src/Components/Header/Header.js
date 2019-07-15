eba36dc49c126998b36b17a29c84e99ee27ef4d4
Byeongjin Kang <byeongjinkang@Byeongjinui-MacBookPro.local>
2019-07-15 16:55:11 +0900

    icons changed (fontawesome -> background-image)

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,26 +1,20 @@
 import React from "react";
 import "./header.scss";
 import Button from "Components/Button";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSocks, faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="pageHeader">
       <span className="myAccountWrap">
-        <span className="myAccountIcon">
-          <FontAwesomeIcon icon={faSocks} size="2x" />
-        </span>
+        <span className="myAccountIcon"></span>
         <Link to="/mypage">
           <Button className="myAccountBtn" text="My Account" />
         </Link>
       </span>
       <span className="logo"></span>
       <span className="cartWrap">
-        <span className="cartIcon">
-          <FontAwesomeIcon icon={faShoppingCart} size="2x" />
-        </span>
+        <span className="cartIcon"></span>
         <Button className="cartBtn" text="Cart" />
       </span>
     </div>

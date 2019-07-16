def3d99c6f2d5b1e922c11ad184b43ff601342e2
Byeongjin Kang <byeongjinkang@Byeongjinui-MacBookPro.local>
2019-07-17 00:10:23 +0900

    WIP

--- src/Pages/MyPage/MyAccount/MyAccount.js
+++ src/Pages/MyPage/MyAccount/MyAccount.js
@@ -56,8 +56,43 @@ class MyAccount extends React.Component {
                   onClick={e => this.handleClick(e)}
                 />
                 <div
-                  className={`myAccountExplanation rewardsExplanataion ${
+                  className={`myAccountExplanation viewRewardsExplanation ${
                     this.state.clickedBtn === "viewRewards"
+                      ? "showDetail"
+                      : "hidden"
+                  }`}
+                >
+                  <div className="designerLevelWrap">
+                    <p>당신은 Rookie Designer입니다</p>
+                    <p>포인트를 적립하여 다음 Level의 Designer가 되어보세요</p>
+                  </div>
+                  <div className="designerLevelWrap">
+                    <p>당신은 Junior Designer 입니다</p>
+                    <p>매월 5%의 할인쿠폰이 발급됩니다.</p>
+                    <p>포인트를 적립하여 다음 Level의 Designer가 되어보세요</p>
+                  </div>
+                  <div className="designerLevelWrap">
+                    <p>당신은 Senior Designer 입니다</p>
+                    <p>매월 10%의 할인쿠폰이 발급됩니다.</p>
+                    <p>포인트를 적립하여 다음 Level의 Designer가 되어보세요</p>
+                  </div>
+                  <div className="designerLevelWrap">
+                    <p>당신은 Master Designer 입니다</p>
+                    <p>매월 20%의 할인쿠폰이 발급됩니다.</p>
+                    <p>
+                      WeSocks와 함께 Statement를 만드는 당신의 삶을 응원합니다.
+                    </p>
+                  </div>
+                </div>
+                <Button
+                  className="learnMoreBtn"
+                  name="learnMore"
+                  text="LEARN MORE"
+                  onClick={e => this.handleClick(e)}
+                />
+                <div
+                  className={`myAccountExplanation learnMoreExplanation ${
+                    this.state.clickedBtn === "learnMore"
                       ? "showDetail"
                       : "hidden"
                   }`}
@@ -94,12 +129,6 @@ class MyAccount extends React.Component {
                     </p>
                   </div>
                 </div>
-                <Button
-                  className="learnMoreBtn"
-                  name="learnMore"
-                  text="LEARN MORE"
-                  onClick={e => this.handleClick(e)}
-                />
               </div>
             </div>
             <div className="clubImage"></div>

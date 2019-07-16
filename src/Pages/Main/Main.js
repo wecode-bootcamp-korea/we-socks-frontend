3f028b4be512975fc7e795cf4f8c5c550357aa33
Georgee-lee <rjsahdia3@gmail.com>
2019-07-16 10:28:05 +0900

    make other component

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -8,7 +8,7 @@ import kidsImg from "assets/images/kids.jpg";
 import dressedImg from "assets/images/dressed.jpg";
 import athletic from "assets/images/athletic.jpg";
 
-const Main = () => {
+const Main = props => {
   return (
     <>
       <Header />
@@ -18,7 +18,6 @@ const Main = () => {
             pathname: "/itemlist",
             query: { kind: "kids" }
           }}
-          className="move-btn"
           style={{ textDecoration: "none" }}
         >
           <Square image_url={kidsImg} title="KIDS" />
@@ -28,7 +27,6 @@ const Main = () => {
             pathname: "/itemlist",
             query: { kind: "casual" }
           }}
-          className="move-btn"
           style={{ textDecoration: "none" }}
         >
           <Square image_url={casualImg} title="CASUAL" />
@@ -38,7 +36,6 @@ const Main = () => {
             pathname: "/itemlist",
             query: { kind: "dressed" }
           }}
-          className="move-btn"
           style={{ textDecoration: "none" }}
         >
           <Square image_url={dressedImg} title="DRESSED" />
@@ -48,11 +45,125 @@ const Main = () => {
             pathname: "/itemlist",
             query: { kind: "athletic" }
           }}
-          className="move-btn"
           style={{ textDecoration: "none" }}
         >
           <Square image_url={athletic} title="ATHLETIC" />
         </Link>
+      </div>
+      <div className="mainFooter">
+        <div className="footerItem">
+          <h2>
+            <img
+              src="https://www.happysocks.com/media/wysiwyg/Generic/icon-usp/smiley_1x.png"
+              sizes="35px"
+              height="35"
+              alt="Smiley icon"
+            />
+          </h2>
+          <h2>SO MUCH MORE THAN SOCKS</h2>
+          <p>
+            Discover our wide selection of{" "}
+            <b>
+              <span>Limited Edition</span>
+            </b>{" "}
+            styles,
+            <b>
+              <span>Dressed</span>
+            </b>
+            ,
+            <b>
+              <span>Athletic</span>
+            </b>
+            ,
+            <b>
+              <span>underwear</span>
+            </b>
+            for men and women, gifts and Hysteria for women.
+          </p>
+        </div>
+        <div className="footerItem">
+          <h2>
+            <img
+              src="https://www.happysocks.com/media/wysiwyg/Generic/icon-usp/heart_1x.png"
+              sizes="35px"
+              height="35"
+              alt="Smiley icon"
+            />
+          </h2>
+          <h2>THE PERFECT GIFT</h2>
+          <span>
+            Classic
+            <b>
+              <span>dotted</span>
+            </b>{" "}
+            and{" "}
+            <b>
+              <span>striped</span>
+            </b>{" "}
+            socks for dad, birthday gifts and Christmas presents in fun colors
+            and patterns, there’s a style for every occasion.{" "}
+          </span>
+        </div>
+        <div className="footerItem">
+          <h2>
+            <img
+              src="https://www.happysocks.com/media/wysiwyg/Generic/icon-usp/smiley_1x.png"
+              sizes="35px"
+              height="35"
+              alt="Smiley icon"
+            />
+          </h2>
+          <h2>SO MUCH MORE THAN SOCKS</h2>
+          <p>
+            Discover our wide selection of{" "}
+            <b>
+              <span>Limited Edition</span>
+            </b>{" "}
+            styles,
+            <b>
+              <span>Dressed</span>
+            </b>
+            ,
+            <b>
+              <span>Athletic</span>
+            </b>
+            ,
+            <b>
+              <span>underwear</span>
+            </b>
+            for men and women, gifts and Hysteria for women.
+          </p>
+        </div>
+        <div className="footerItem">
+          <h2>
+            <img
+              src="https://www.happysocks.com/media/wysiwyg/Generic/icon-usp/smiley_1x.png"
+              sizes="35px"
+              height="35"
+              alt="Smiley icon"
+            />
+          </h2>
+          <h2>SO MUCH MORE THAN SOCKS</h2>
+          <p>
+            Discover our wide selection of{" "}
+            <b>
+              <span>Limited Edition</span>
+            </b>{" "}
+            styles,
+            <b>
+              <span>Dressed</span>
+            </b>
+            ,
+            <b>
+              <span>Athletic</span>
+            </b>
+            ,
+            <b>
+              <span>underwear</span>
+            </b>
+            for men and women, gifts and Hysteria for women.
+          </p>
+        </div>
       </div>
     </>
   );

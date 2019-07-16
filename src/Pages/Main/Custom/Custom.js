0baaf346669ba54407b9f37a66dadab47a6e31f8
Byeongjin Kang <byeongjinkang@Byeongjinui-MacBookPro.local>
2019-07-16 15:16:51 +0900

    ColorPicker implemented

--- src/Pages/Main/Custom/Custom.js
+++ src/Pages/Main/Custom/Custom.js
@@ -1,17 +1,52 @@
 import React from "react";
 import "./custom.scss";
 import Header from "Components/Header";
+import InputBox from "Components/InputBox";
 
 class Custom extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      pickedColor: "white"
+    };
+  }
+
+  changeSocksColor(e) {
+    this.setState({
+      pickedColor: e.target.value
+    });
+  }
+
   render() {
     return (
       <>
         <Header />
         <div className="customRoot">
-          <canvas></canvas>
-          <div className="colorPicker">color</div>
-          <div className="patternPicker">pattern</div>
-          <div className="imagePicker">image</div>
+          <div
+            className="socksContainer"
+            style={{ backgroundColor: this.state.pickedColor }}
+          ></div>
+          <div className="chooseWrap">
+            <div className="chooseColor">
+              <p>Choose Color</p>
+              <div className="colorPickerContainer">
+                <InputBox
+                  type="color"
+                  className="colorPicker"
+                  handleChange={e => this.changeSocksColor(e)}
+                />
+              </div>
+            </div>
+            <div className="choosePattern">
+              <p>Choose Pattern</p>
+              <div className="patternPicker"></div>
+            </div>
+            <div className="chooseImage">
+              <p>Choose Image</p>
+              <div className="imagePicker"></div>
+            </div>
+          </div>
         </div>
       </>
     );

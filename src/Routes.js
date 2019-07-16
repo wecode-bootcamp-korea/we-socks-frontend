fa8c1bb997aa291818d05c6692e5a418753228aa
Byeongjin Kang <byeongjinkang@Byeongjinui-MacBookPro.local>
2019-07-16 23:56:38 +0900

    deleted Modal.js and implemented each modal within its component

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Main from "./Pages/Main";
+import MyPage from "Pages/MyPage";
 import Itemlist from "./Pages/Itemlist";
 import Signup from "./Pages/Signup";
 import Login from "./Pages/Login";
+<<<<<<< HEAD
 import Custom from "Pages/Main/Custom";
-
+=======
+>>>>>>> 66c1f21... Modal wip
+import OrderDetail from "Pages/MyPage/OrderHistory/OrderDetail";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 class Routes extends React.Component {
@@ -13,10 +17,12 @@ class Routes extends React.Component {
       <Router>
         <Switch>
           <Route exact path="/" component={Main} />
+          <Route exact path="/mypage" component={MyPage} />
           <Route exact path="/itemlist" component={Itemlist} />
           <Route exact path="/custom" component={Custom} />
           <Route exact path="/signup" component={Signup} />
           <Route exact path="/login" component={Login} />
+          <Route exact path="/orderdetail" component={OrderDetail} />
         </Switch>
       </Router>
     );

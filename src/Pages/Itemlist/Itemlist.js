4722e4457141a346dd2c8a90e9878faa69b7a628
Georgee-lee <rjsahdia3@gmail.com>
2019-07-16 15:59:52 +0900

    item 페이지 구현

--- src/Pages/Itemlist/Itemlist.js
+++ src/Pages/Itemlist/Itemlist.js
@@ -1,15 +1,25 @@
 import React from "react";
 import Header from "Components/Header";
+import SockItem from "Components/SockItem";
 import "./itemlist.scss";
 
 const itemlist = props => {
-  const category = props.location.query.kind;
+  // const category = props.location.query.kind;
 
   return (
     <>
       <Header />
-      <div>
-        <p>123</p>
+      <div className="lineBox"></div>
+      <div className="itemlist">
+        <div className="listHeader">
+          <h3>Choose Your Socks type</h3>
+        </div>
+        <div className="list">
+          <SockItem image="https://cdn.shopify.com/s/files/1/0158/7614/products/black_pedla_lightweight_sock_800x.png?v=1544163225" />
+          <SockItem image="https://cdn.shopify.com/s/files/1/0158/7614/products/black_pedla_lightweight_sock_800x.png?v=1544163225" />
+          <SockItem image="https://cdn.shopify.com/s/files/1/0158/7614/products/black_pedla_lightweight_sock_800x.png?v=1544163225" />
+          <SockItem image="https://cdn.shopify.com/s/files/1/0158/7614/products/black_pedla_lightweight_sock_800x.png?v=1544163225" />
+        </div>
       </div>
     </>
   );

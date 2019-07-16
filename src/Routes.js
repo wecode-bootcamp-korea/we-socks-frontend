ece82c72377585ec3058c49ce4be82486e4944b3
Byeongjin Kang <byeongjinkang@Byeongjinui-MacBookPro.local>
2019-07-16 23:56:38 +0900

    MyPage started to communicate with the server

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Main from "./Pages/Main";
+import MyPage from "Pages/MyPage";
 import Itemlist from "./Pages/Itemlist";
 import Signup from "./Pages/Signup";
 import Login from "./Pages/Login";
 import Custom from "Pages/Main/Custom";
 
+import OrderDetail from "Pages/MyPage/OrderHistory/OrderDetail";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 class Routes extends React.Component {
@@ -13,10 +15,12 @@ class Routes extends React.Component {
       <Router>
         <Switch>
           <Route exact path="/" component={Main} />
+          <Route exact path="/mypage" component={MyPage} />
           <Route exact path="/itemlist" component={Itemlist} />
           <Route exact path="/custom" component={Custom} />
           <Route exact path="/signup" component={Signup} />
           <Route exact path="/login" component={Login} />
+          <Route exact path="/orderdetail" component={OrderDetail} />
         </Switch>
       </Router>
     );

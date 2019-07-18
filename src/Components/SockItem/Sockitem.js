6e5d5daabe5fe5a8d85677e5976099343a173b95
Georgee-lee <rjsahdia3@gmail.com>
2019-07-19 06:53:06 +0900

    main 화면 재조정, hover 시 opacity 추가

--- src/Components/SockItem/Sockitem.js
+++ src/Components/SockItem/Sockitem.js
@@ -1,18 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./sockitem.scss";
 
 const SockItem = ({ image }) => {
   return (
-    <div className="item">
-      <div
-        className="itemImage"
-        style={{ backgroundImage: `url(${image})` }}
-      ></div>
-      <div className="customLabel">
-        <span className="miniLogo"></span>
-        <span className="miniLabel">Customize</span>
+    <Link to="/custom">
+      <div className="item">
+        <div
+          className="itemImage"
+          style={{ backgroundImage: `url(${image})` }}
+        ></div>
+        <div className="customLabel">
+          <span className="miniLogo"></span>
+          <span className="miniLabel">Customize</span>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 

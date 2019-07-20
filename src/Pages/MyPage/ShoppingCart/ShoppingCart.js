1618de998b80c258998117582f79edeea3921137
Byeongjin Kang <byeongjinkang@Byeongjinui-MacBookPro.local>
2019-07-20 15:12:33 +0900

    A list for the shopping cart has been connected with an API

--- src/Pages/MyPage/ShoppingCart/ShoppingCart.js
+++ src/Pages/MyPage/ShoppingCart/ShoppingCart.js
@@ -4,20 +4,35 @@ import Header from "Components/Header";
 import Footer from "Components/Footer";
 import Button from "Components/Button";
 import Select from "Components/Select";
+import SockItem from "Components/SockItem";
 import "./shoppingCart.scss";
 
+const categoryArr = ["Kids", "Casual", "Dressed", "Athletic"];
+const typeArr = ["noShow", "ankle", "mid", "high"];
+
 class ShoppingCart extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      cartArr: []
+    };
+  }
   removeItem = () => {};
 
   componentDidMount() {
     axios
-      .post("http://10.58.5.85:8002/mypage/my_basket", { buyer: "bj" })
+      .post("http://10.58.6.101:8001/mypage/my_cart", { buyer: "bj" })
       .then(response => {
-        console.log(response);
+        this.setState({
+          cartArr: response.data[0].my_basket_list
+        });
       });
   }
 
   render() {
+    const { cartArr } = this.state;
+    console.log(cartArr);
     return (
       <>
         <Header />
@@ -26,55 +41,40 @@ class ShoppingCart extends React.Component {
           <div className="shoppingCartContainer">
             <div className="leftBox">
               <ul className="shoppingCartTotal">
-                <li className="shoppingCartList">
-                  <div className="productImage"></div>
-                  <div className="productMiddleContainer">
-                    <div className="productName">Tacos Everywhere</div>
-                    <div className="productCategoryAndType">Casual High</div>
-                    <div className="productPrice">가격: 8,000</div>
-                    <div className="pointsPrediction">예상포인트: 600</div>
-                  </div>
-                  <div className="productRightContainer">
-                    <Select
-                      className="count"
-                      name="count"
-                      makeSelection={this.handleCount}
-                      ref_array={[1, 2, 3, 4, 5]}
-                    />
+                {cartArr.map((el, idx) => (
+                  <li className="shoppingCartList">
+                    <div className="productImage">
+                      <SockItem
+                        type={typeArr[el.design.main_type - 1]}
+                        color={el.design.color}
+                        pattern={el.design.pattern}
+                        view="side"
+                      />
+                    </div>
+                    <div className="productMiddleContainer">
+                      <div className="productCategoryAndType">{`${
+                        categoryArr[el.design.category]
+                      } ${typeArr[el.design.main_type - 1]}`}</div>
+                      <div className="productPrice">가격: 8,000</div>
+                      <div className="pointsPrediction">예상포인트: 600</div>
+                    </div>
+                    <div className="productRightContainer">
+                      <Select
+                        className="count"
+                        name="count"
+                        makeSelection={this.handleCount}
+                        ref_array={[1, 2, 3, 4, 5]}
+                      />
 
-                    <Button
-                      className="removeItemBtn"
-                      name="removeItemBtn"
-                      text="삭제"
-                      onClick={this.removeItem}
-                    />
-                  </div>
-                </li>
-
-                <li className="shoppingCartList">
-                  <div className="productImage"></div>
-                  <div className="productMiddleContainer">
-                    <div className="productName">Tacos Everywhere</div>
-                    <div className="productCategoryAndType">Casual High</div>
-                    <div className="productPrice">가격: 8,000</div>
-                    <div className="pointsPrediction">예상포인트: 600</div>
-                  </div>
-                  <div className="productRightContainer">
-                    <Select
-                      className="count"
-                      name="count"
-                      makeSelection={this.handleCount}
-                      ref_array={[1, 2, 3, 4, 5]}
-                    />
-
-                    <Button
-                      className="removeItemBtn"
-                      name="removeItemBtn"
-                      text="삭제"
-                      onClick={this.removeItem}
-                    />
-                  </div>
-                </li>
+                      <Button
+                        className="removeItemBtn"
+                        name="removeItemBtn"
+                        text="삭제"
+                        onClick={this.removeItem}
+                      />
+                    </div>
+                  </li>
+                ))}
               </ul>
             </div>
             <div className="rightBox">

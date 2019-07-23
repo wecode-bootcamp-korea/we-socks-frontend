d4d0873d50727279a269141d6f85807754363fe7
Georgee-lee <rjsahdia3@gmail.com>
2019-07-23 20:51:57 +0900

    itemlist preview 기능 구현 완료

--- src/Pages/Itemlist/Itemlist.js
+++ src/Pages/Itemlist/Itemlist.js
@@ -1,33 +1,82 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 import SockPreview from "Components/SockPreview";
 import "./itemlist.scss";
 
-const itemlist = props => {
-  return (
-    <div className="itemlist">
-      <div className="listHeader">
-        <h3>Choose Your Socks type</h3>
+const API_URL = "http://10.58.5.28:8001";
+
+class itemlist extends Component {
+  state = {
+    noShowPreviewArr: [],
+    anklePreviewArr: [],
+    midPreviewArr: [],
+    highPreviewArr: []
+  };
+
+  componentDidMount = async () => {
+    const noshow = await axios.post(`${API_URL}/main/most_wished_list`, {
+      main_type: "1"
+    });
+    const ankle = await axios.post(`${API_URL}/main/most_wished_list`, {
+      main_type: "2"
+    });
+    const mid = await axios.post(`${API_URL}/main/most_wished_list`, {
+      main_type: "3"
+    });
+    const high = await axios.post(`${API_URL}/main/most_wished_list`, {
+      main_type: "4"
+    });
+
+    this.setState({
+      noShowPreviewArr: noshow.data,
+      anklePreviewArr: ankle.data,
+      midPreviewArr: mid.data,
+      highPreviewArr: high.data
+    });
+  };
+
+  render() {
+    const {
+      noShowPreviewArr,
+      anklePreviewArr,
+      midPreviewArr,
+      highPreviewArr
+    } = this.state;
+
+    return (
+      <div className="itemlist">
+        <div className="listHeader">
+          <h3>Choose Your Socks type</h3>
+        </div>
+        <div className="list">
+          <SockPreview
+            defaultType="0"
+            defaultColor="none"
+            defaultPattern="0"
+            preview={noShowPreviewArr}
+          />
+          <SockPreview
+            defaultType="1"
+            defaultColor="none"
+            defaultPattern="0"
+            preview={anklePreviewArr}
+          />
+          <SockPreview
+            defaultType="2"
+            defaultColor="none"
+            defaultPattern="0"
+            preview={midPreviewArr}
+          />
+          <SockPreview
+            defaultType="3"
+            defaultColor="none"
+            defaultPattern="0"
+            preview={highPreviewArr}
+          />
+        </div>
       </div>
-      <div className="list">
-        <SockPreview
-          defaultType="noShow"
-          defaultColor="none"
-          defaultPattern="0"
-        />
-        <SockPreview
-          defaultType="ankle"
-          defaultColor="none"
-          defaultPattern="0"
-        />
-        <SockPreview defaultType="mid" defaultColor="none" defaultPattern="0" />
-        <SockPreview
-          defaultType="high"
-          defaultColor="none"
-          defaultPattern="0"
-        />
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default itemlist;

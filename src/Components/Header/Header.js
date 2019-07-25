578736b4bed33550e99e77fa8aa0205646e48bfb
Byeongjin Kang <byeongjinkang@Byeongjinui-MacBookPro.local>
2019-07-25 23:28:18 +0900

    Header Component responsive complete, closes #57

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./header.scss";
 import Button from "Components/Button";
 import { Link } from "react-router-dom";
+import { TOKEN_KEY } from "config";
 import AddedToCartMessage from "Components/AddedToCartMessage";
 class Header extends React.Component {
   constructor() {
@@ -27,47 +28,57 @@ class Header extends React.Component {
     );
   };
 
+  componentDidMount = () => {
+    const token = localStorage.getItem(TOKEN_KEY);
+    console.log(token);
+  };
+
   render() {
     const { cartBtnClicked } = this.state;
     return (
       <>
         <header className="pageHeader">
           <div className="headerTop">
-            <div className="myAccountWrap">
-              <div className="myAccountIcon"></div>
-              <Link to="/mypage">
-                <Button className="myAccountBtn" text="My Account" />
-              </Link>
-            </div>
-            <Link to="/" className="logo">
-              <span className="logoIcon"></span>
+            <Link to="/" className="logoWrap">
+              <div className="logoIcon"></div>
             </Link>
-            <div className="cartWrap">
-              <div className="cartIcon"></div>
-              <Link to="/shoppingcart">
-                <Button
-                  className="cartBtn"
-                  text="Cart"
-                  onClick={this.handleCartBtnClicked}
-                />
-              </Link>
-            </div>
-            <AddedToCartMessage showMessage={cartBtnClicked} />
-          </div>
-          <div className="headerBottom">
-            <div className="kids">
-              <span>KIDS</span>
-            </div>
-            <div className="casual">
-              <span>CASUAL</span>
-            </div>
-            <div className="dressed">
-              <span>DRESSED</span>
-            </div>
-            <div className="athletic">
-              <span>ATHLETIC</span>
+            <div className="headerBtnWrap">
+              <div className="myAccountWrap">
+                <div className="myAccountIcon"></div>
+                <Link to="/mypage">
+                  <Button className="myAccountBtn" text="My Account" />
+                </Link>
+              </div>
+
+              <div className="cartWrap">
+                <div className="cartIcon"></div>
+                <Link to="/shoppingcart">
+                  <Button
+                    className="cartBtn"
+                    text="Cart"
+                    onClick={this.handleCartBtnClicked}
+                  />
+                </Link>
+              </div>
+              <AddedToCartMessage showMessage={cartBtnClicked} />
             </div>
           </div>
+          <Link to="/main">
+            <div className="headerBottom">
+              <div className="kids">
+                <Button text="KIDS" />
+              </div>
+              <div className="casual">
+                <Button text="CASUAL" />
+              </div>
+              <div className="dressed">
+                <Button text="DRESSED" />
+              </div>
+              <div className="athletic">
+                <Button text="ATHLETIC" />
+              </div>
+            </div>
+          </Link>
         </header>
       </>
     );
